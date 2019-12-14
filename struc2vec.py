0146b2cc172b6840c7fb111042d8ed7d6ee18a2d
Bijay Gaudel <gaudelbijay10@gmail.com>
2019-12-14 16:16:27 +0800

    ordered_degree_sequence

--- struc2vec.py
+++ struc2vec.py
@@ -50,4 +50,31 @@ class Struc2Vec():
         visited[root] = True 
 
         while(len(queue)>0 and level <= max_num_layers):
-            
+            count = len(queue)
+            if self.opt1_reduce_len:
+                degree_list = {}
+            else:
+                degree_list = []
+            while count>0:
+                top = queue.popleft()
+                node = self.idx2node[top]
+                degree = len(self.graph[node])
+
+                if self.opt1_reduce_len:
+                    degree_list[degree] = degree_list.get(degree,0)+1
+                else: degree_list.append(degree)
+                for nei in self.graph[node]:
+                    nei_idx = node2idx[nei]
+                    if not visited[nei_idx]:
+                        visited[nei_idx]=True
+                        queue.append(nei)
+                count -=1
+            if self.opt1_reduce_len:
+                ordered_degree_list = [(degree,freq) for degree,freq in degree_list.items()]
+                ordered_degree_list.sort(key=lambda x: x[0])
+            else:
+                orderd_degree_list = sorted(degree_list)
+            ordered_degree_sequence_dict[level] = orderd_degree_list
+            level += 1
+
+        return ordered_degree_sequence_dict
