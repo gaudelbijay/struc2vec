2679fee86f8159e963ca8fe52b756252c5d13b11
Bijay Gaudel <gaudelbijay10@gmail.com>
2019-12-17 15:45:35 +0800

    dictionary partition

--- utils.py
+++ utils.py
@@ -5,3 +5,18 @@ def preprocess_nxgraph(graph):
         node2idx[node] = i 
         idx2node.append(node)
     return idx2node,node2idx 
+
+def partition_dict(vertices,workers):
+    batch_size = (len(vertices)-1)//workers +1 
+    part_list = []
+    part = []
+    count = 0 
+    for v,nbs in vertices.items():
+        part.append((v,nbs))
+        count +=1 
+        if count%batch_size == 0:
+            part_list.append(part)
+            part=[]
+    if len(part)>0:
+        part_list.append(part)
+    return part_list
